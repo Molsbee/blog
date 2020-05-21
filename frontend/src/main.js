25b71db4e30bd86df70d0f613c23c65bd44b82ae
molsbee <molsbee@gmail.com>
2020-05-21 10:20:03 -0500

    Moved router into main.js as having additional location to check was annoying for size of router at this point.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
-import router from './router'
+import Home from './views/Home.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('./views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
 
 new Vue({
   router,
