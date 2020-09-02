b659e09e2919370e03c828e7b8fdc3ab673efcb3
molsbee <molsbee@gmail.com>
2020-09-02 14:31:04 -0500

    Implementing more of the write blog page.

--- frontend/src/views/WriteBlog.vue
+++ frontend/src/views/WriteBlog.vue
@@ -1,62 +1,85 @@
 <template>
-    <div class="create">
-        <v-row>
-            <h3>Create a new blog page</h3>
-        </v-row>
-        <v-row>
-            <v-text-field label="Title" v-bind="blog_title"></v-text-field>
-        </v-row>
-        <v-row>
-            <v-tabs>
-                <v-tab @click="write">Write</v-tab>
-                <v-tab @click="showHTML">Preview</v-tab>
-            </v-tabs>
-        </v-row>
-        <v-row v-show="!show_html" style="height: 100%">
-            <v-textarea
-                    outlined
-                    name="article"
-                    v-model="blog_markdown"
-                    rows="18"
-            ></v-textarea>
-        </v-row>
-        <v-row v-show="show_html" style="height: 100%">
-            <div v-html="blog_html"></div>
-        </v-row>
-        <v-row>
-            <v-spacer></v-spacer>
-            <v-btn @click="submit">Save Article</v-btn>
-        </v-row>
-    </div>
+  <div class="create">
+    <v-row>
+      <h3>Create a new blog page</h3>
+    </v-row>
+    <v-row>
+      <v-text-field label="Title" v-bind="title"></v-text-field>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-text-field label="Author" v-bind="author"></v-text-field>
+      </v-col>
+      <v-col>
+        <v-checkbox label="Publish" v-bind="publish"></v-checkbox>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-tabs>
+        <v-tab @click="write">Write</v-tab>
+        <v-tab @click="showHTML">Preview</v-tab>
+      </v-tabs>
+    </v-row>
+    <v-row v-show="!show_html" style="height: 100%">
+      <v-textarea
+          outlined
+          name="article"
+          v-model="blog_markdown"
+          rows="18"
+      ></v-textarea>
+    </v-row>
+    <v-row v-show="show_html" style="height: 100%">
+      <div v-html="blog_html"></div>
+    </v-row>
+    <v-row>
+      <v-spacer></v-spacer>
+      <v-card-text v-show="errored" class="error--text">Failed to save article</v-card-text>
+      <v-btn @click="submit">Save Article</v-btn>
+    </v-row>
+  </div>
 </template>
 
 <script>
-    import showdown from 'showdown'
+import showdown from 'showdown'
+import axios from 'axios'
 
-    let converter = new showdown.Converter()
-    export default {
-        name: 'WriteBlog',
-        data() {
-            return {
-                blog_title: "",
-                blog_markdown: "",
-                blog_html: "",
-                show_html: false
-            }
-        },
-        methods: {
-            write: function() {
-                this.show_html = false
-            },
-            showHTML: function() {
-                console.log(this)
-                console.log(this.blog_markdown)
-                this.blog_html = converter.makeHtml(this.blog_markdown)
-                this.show_html = true
-            },
-            submit: function() {
-                // Implement
-            }
-        }
+let converter = new showdown.Converter()
+export default {
+  name: 'WriteBlog',
+  data() {
+    return {
+      title: "",
+      author: "",
+      blog_markdown: "",
+      blog_html: "",
+      show_html: false,
+      errored: false,
+      publish: false
     }
+  },
+  methods: {
+    write: function () {
+      this.show_html = false
+    },
+    showHTML: function () {
+      this.blog_html = converter.makeHtml(this.blog_markdown)
+      this.show_html = true
+    },
+    submit: function () {
+      let request = {
+        Title: this.title,
+        Content: this.blog_markdown,
+        Author: this.author,
+        Published: this.publish
+      }
+      axios
+          .post("/api/articles", request)
+          .then(response => { this.$router.push('/') })
+          .catch(error => {
+            console.log(error)
+            this.errored = true
+          })
+    }
+  }
+}
 </script>
